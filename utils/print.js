65fbc2acb243cd2dd7c6385cb3d3c75ed65153ee
Robin Fehr <info@robinfehr.ch>
2018-11-01 18:36:43 +0100

    do not delete log progress

--- utils/print.js
+++ utils/print.js
@@ -1,8 +1,6 @@
 const readline = require('readline');
 module.exports = {
   printProgress:  (progress) => {
-    readline.clearLine(process.stdout, 0)
-    readline.cursorTo(process.stdout, 0)
     process.stdout.write(progress + '%');
   }
 }
